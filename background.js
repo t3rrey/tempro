31741aeb3600b5d5cabcb5d7165f4c735b4357ee
Lachlan <lachlanterrey@yahoo.com>
2022-02-02 22:02:41 +1100

    Popup box styling draft complete

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
+let color = "#3AA757";
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color });
+    console.log("Worked");
+});
+
 const getCurrentTab = async() => {
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
+    console.log(tab);
     return tab;
 };
 
-modules.export = { getCurrentTab };
+getCurrentTab();
