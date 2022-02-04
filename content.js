931cd058bf1504ae11950d53a164c80a3c478ac8
Lachlan <lachlanterrey@yahoo.com>
2022-02-04 11:16:11 +1100

    Server Setup

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 const addButton = () => {
-    const insert = document.querySelector(".nav-tabs");
+    const insert = document.querySelector("NZAU7CsuZsMeMQB8zYUu");
     insert.insertAdjacentHTML("afterbegin", "<button>Add Song</button");
     console.log("Element Added");
 };
